7c88f9a702f1206e29fd4ccf8135bccbf6320020
HamzaTote <119054111+HamzaTote@users.noreply.github.com>
2023-04-08 14:17:34 +0000

    Update SpeakGoodBye.js

--- mdule4/SpeakGoodBye.js
+++ mdule4/SpeakGoodBye.js
@@ -2,7 +2,7 @@
 	var speakWord = "Good Bye";
 	var byeSpeaker = {
 	  speak: function (name){
-	    console.log(speakWord + " " +name);
+	    console.log(speakWord + " " + name);
 	};
 	window.byeSpeaker = byeSpeaker;
 })(window);
